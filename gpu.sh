25c47857775334fbe6f082b2eb9de4601e4989af
Max! <admin@cloudydev.net>
2023-05-19 19:53:32 +0200

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,7 @@
+
 #!/bin/bash -e
 
-# Sourece: 
+# Sourece:
 https://github.com/selkies-project/docker-nvidia-glx-desktop/blob/main/entrypoint.sh
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -16,7 +17,7 @@ export APPIMAGE_EXTRACT_AND_RUN=1
 export DISPLAY=:0
 export XDG_RUNTIME_DIR=/tmp/runtime-user
 #export PULSE_SERVER=unix:/run/pulse/native
-export 
+export
 LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 # Default environment variables (password is "mypasswd")
@@ -45,7 +46,7 @@ init(){
         sudo /etc/init.d/dbus start
 
         # Change time zone from environment variable
-        sudo ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" | sudo tee 
+        sudo ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" | sudo tee
 /etc/timezone > /dev/null
 
         # This symbolic link enables running Xorg inside a container with `-sharevts`
@@ -53,11 +54,11 @@ init(){
 
         # Allow starting Xorg from a pseudoterminal instead of strictly on a tty console
         if [ ! -f /etc/X11/Xwrapper.config ]; then
-            echo -e "allowed_users=anybody\nneeds_root_rights=yes" | sudo tee 
+            echo -e "allowed_users=anybody\nneeds_root_rights=yes" | sudo tee
 /etc/X11/Xwrapper.config > /dev/null
         fi
         if grep -Fxq "allowed_users=console" /etc/X11/Xwrapper.config; then
-          sudo sed -i "s/allowed_users=console/allowed_users=anybody/;$ a 
+          sudo sed -i "s/allowed_users=console/allowed_users=anybody/;$ a
 needs_root_rights=yes" /etc/X11/Xwrapper.config
         fi
 
@@ -71,17 +72,17 @@ install_driver() {
         # Install NVIDIA userspace driver components including X graphic libraries
         if ! command -v nvidia-xconfig &> /dev/null; then
           # Driver version is provided by the kernel through the container toolkit
-          export DRIVER_VERSION=$(head -n1 </proc/driver/nvidia/version | awk '{print 
+          export DRIVER_VERSION=$(head -n1 </proc/driver/nvidia/version | awk '{print
 $8}')
           cd /tmp
           # If version is different, new installer will overwrite the existing components
           if [ ! -f "/tmp/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run" ]; then
-            # Check multiple sources in order to probe both consumer and datacenter driver 
+            # Check multiple sources in order to probe both consumer and datacenter driver
 versions
-            curl -fsL -O 
-"https://us.download.nvidia.com/XFree86/Linux-x86_64/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run" 
-|| curl -fsL -O 
-"https://us.download.nvidia.com/tesla/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run" 
+            curl -fsL -O
+"https://us.download.nvidia.com/XFree86/Linux-x86_64/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run"
+|| curl -fsL -O
+"https://us.download.nvidia.com/tesla/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run"
 || { echo "Failed NVIDIA GPU driver download. Exiting."; exit 1; }
           fi
           # Extract installer before installing
@@ -95,14 +96,14 @@ versions
                             --no-nvidia-modprobe \
                             --no-rpms \
                             --no-backup \
-                            --no-check-for-alternate-installs
+                            --no-check-for-alternate-installs || true
           sudo rm -rf /tmp/NVIDIA* && cd ~
 fi
 }
 
 
 find_gpu(){
-        # Get first GPU device if all devices are available or `NVIDIA_VISIBLE_DEVICES` is 
+        # Get first GPU device if all devices are available or `NVIDIA_VISIBLE_DEVICES` is
 not set
         if [ "$NVIDIA_VISIBLE_DEVICES" == "all" ]; then
           export GPU_SELECT=$(sudo nvidia-smi --query-gpu=uuid --format=csv | sed -n 2p)
@@ -110,7 +111,7 @@ not set
           export GPU_SELECT=$(sudo nvidia-smi --query-gpu=uuid --format=csv | sed -n 2p)
         # Get first GPU device out of the visible devices in other situations
         else
-          export GPU_SELECT=$(sudo nvidia-smi --id=$(echo "$NVIDIA_VISIBLE_DEVICES" | cut 
+          export GPU_SELECT=$(sudo nvidia-smi --id=$(echo "$NVIDIA_VISIBLE_DEVICES" | cut
 -d ',' -f1) --query-gpu=uuid --format=csv | sed -n 2p)
           if [ -z "$GPU_SELECT" ]; then
             export GPU_SELECT=$(sudo nvidia-smi --query-gpu=uuid --format=csv | sed -n 2p)
@@ -118,18 +119,16 @@ not set
         fi
 
         if [ -z "$GPU_SELECT" ]; then
-          echo "No NVIDIA GPUs detected or nvidia-container-toolkit not configured. 
-Exiting."
+          echo "No NVIDIA GPUs detected or nvidia-container-toolkit not configured. Exiting."
           exit 1
         fi
 
-        # Setting `VIDEO_PORT` to none disables RANDR/XRANDR, do not set this if using 
-datacenter GPUs
+        # Setting `VIDEO_PORT` to none disables RANDR/XRANDR, do not set this if using datacenter GPUs
         if [ "${VIDEO_PORT,,}" = "none" ]; then
           export CONNECTED_MONITOR="--use-display-device=None"
-        # The X server is otherwise deliberately set to a specific video port despite not 
-being plugged to enable RANDR/XRANDR, monitor will display the screen if plugged to the 
-specific port
+        # The X server is otherwise deliberately set to a specific video port despite not
+        # being plugged to enable RANDR/XRANDR, monitor will display the screen if plugged to the
+        # specific port
         else
           export CONNECTED_MONITOR="--connected-monitor=${VIDEO_PORT}"
         fi
@@ -137,38 +136,39 @@ specific port
 
 create_xorg_conf(){
 
-        # Bus ID from nvidia-smi is in hexadecimal format, should be converted to decimal 
-format which Xorg understands, required because nvidia-xconfig doesn't work as intended in 
-a container
-        HEX_ID=$(sudo nvidia-smi --query-gpu=pci.bus_id --id="$GPU_SELECT" --format=csv | 
+        # Bus ID from nvidia-smi is in hexadecimal format, should be converted to decimal
+        # format which Xorg understands, required because nvidia-xconfig doesn't work as intended in
+        # a container
+        HEX_ID=$(sudo nvidia-smi --query-gpu=pci.bus_id --id="$GPU_SELECT" --format=csv |
 sed -n 2p)
         IFS=":." ARR_ID=($HEX_ID)
         unset IFS
         BUS_ID=PCI:$((16#${ARR_ID[1]})):$((16#${ARR_ID[2]})):$((16#${ARR_ID[3]}))
 
-        # A custom modeline should be generated because there is no monitor to fetch this 
-information normally
+        # A custom modeline should be generated because there is no monitor to fetch this
+        # information normally
         export MODELINE=$(cvt -r "${SIZEW}" "${SIZEH}" "${REFRESH}" | sed -n 2p)
 
         # Generate /etc/X11/xorg.conf with nvidia-xconfig
-        sudo nvidia-xconfig --virtual="${SIZEW}x${SIZEH}" --depth="$CDEPTH" --mode=$(echo 
-"$MODELINE" | awk '{print $2}' | tr -d '"') --allow-empty-initial-configuration 
---no-probe-all-gpus --busid="$BUS_ID" --no-multigpu --no-sli --no-base-mosaic 
+        sudo nvidia-xconfig --virtual="${SIZEW}x${SIZEH}" --depth="$CDEPTH" --mode=$(echo
+"$MODELINE" | awk '{print $2}' | tr -d '"') --allow-empty-initial-configuration
+--no-probe-all-gpus --busid="$BUS_ID" --no-multigpu --no-sli --no-base-mosaic
 --only-one-x-screen ${CONNECTED_MONITOR}
 
-        # Guarantee that the X server starts without a monitor by adding more options to 
-the configuration
-        sudo sed -i '/Driver\s\+"nvidia"/a\    Option         "ModeValidation" 
-"NoMaxPClkCheck, NoEdidMaxPClkCheck, NoMaxSizeCheck, NoHorizSyncCheck, NoVertRefreshCheck, 
-NoVirtualSizeCheck, NoExtendedGpuCapabilitiesCheck, NoTotalSizeCheck, NoDualLinkDVICheck, 
-NoDisplayPortBandwidthCheck, AllowNon3DVisionModes, AllowNonHDMI3DModes, 
-AllowNonEdidModes, NoEdidHDMI2Check, AllowDpInterlaced"\n    Option         "HardDPMS" 
+        # Guarantee that the X server starts without a monitor by adding more options to
+        # the configuration
+        sudo sed -i '/Driver\s\+"nvidia"/a\    Option         "ModeValidation"
+"NoMaxPClkCheck, NoEdidMaxPClkCheck, NoMaxSizeCheck, NoHorizSyncCheck, NoVertRefreshCheck,
+NoVirtualSizeCheck, NoExtendedGpuCapabilitiesCheck, NoTotalSizeCheck, NoDualLinkDVICheck,
+NoDisplayPortBandwidthCheck, AllowNon3DVisionModes, AllowNonHDMI3DModes,
+AllowNonEdidModes, NoEdidHDMI2Check, AllowDpInterlaced"\n    Option         "HardDPMS"
 "False"' /etc/X11/xorg.conf
 
         # Add custom generated modeline to the configuration
         sudo sed -i '/Section\s\+"Monitor"/a\    '"$MODELINE" /etc/X11/xorg.conf
 }
 
-#init
+init
 find_gpu
+install_driver
 create_xorg_conf
